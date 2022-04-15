602178222618dc53519a89a1f5bf25bfc497f4fb
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-04-15 19:24:33 +0900

    파이썬 클린코드 4장 SOLID 원칙
    리스코프 치환 원칙
    인터페이스 분리 원칙
    의존성 역전 원칙

--- python_clean_code/test.py
+++
@@ -1,7 +0,0 @@
-def wrong_user_display(user_metadata: dict={'name': 'john', 'age': 30}):
-    name = user_metadata.pop('name')
-    age = user_metadata.pop('age')
-    return "{} {}".format(name, age)
-
-wrong_user_display()
-
