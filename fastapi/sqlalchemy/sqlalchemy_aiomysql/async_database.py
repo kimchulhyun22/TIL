9737bbfb7f8a98d06542f051cd33551a7b7603f6
kimchulhyun22 <rlacjfgus2@gmail.com>
2022-07-28 18:50:13 +0900

    [UPDATE] async session context 적용

--- fastapi/sqlalchemy/sqlalchemy_aiomysql/async_database.py
+++ fastapi/sqlalchemy/sqlalchemy_aiomysql/async_database.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_scoped_session
 from sqlalchemy.orm import sessionmaker
+from asyncio import current_task
+from session_context import get_session_id
 
 Base = declarative_base()
 
@@ -12,8 +14,13 @@ class AsyncDatabase:
     def __init__(self) -> None:
         self._engine = create_async_engine(DATABASE_URL, echo=True)
 
-        self._async_session = sessionmaker(
+        self._async_session_factory = sessionmaker(
             self._engine, class_=AsyncSession, expire_on_commit=False
+        )
+
+        self._async_session = async_scoped_session(
+            session_factory=self._async_session_factory,
+            scopefunc=get_session_id,
         )
 
     async def create_database(self):
--- fastapi/sqlalchemy/sqlalchemy_aiomysql/main.py
+++ fastapi/sqlalchemy/sqlalchemy_aiomysql/main.py
@@ -1,20 +1,52 @@
 from async_database import database
+from session_context import get_session_id
 from models import *
-
+from asyncio import current_task
+from uuid import uuid4
+from session_context import set_session_context, reset_session_context
 import asyncio
 
 
-async def test():
+async def create_db():
     await database.create_database()
 
-    async with database.async_session() as session:
 
-        session.add(Item(name="test"))
-
-        await session.commit()
-
+async def drop_db():
     await database.drop_database()
 
 
+async def async_db_test():
+    print(current_task())
+    session_id = str(uuid4())
+
+    context = set_session_context(session_id=session_id)
+
+    await create_db()
+
+    session = database.async_session
+
+    await asyncio.gather(test1(session), test2(session))
+
+    await asyncio.gather(test1(session), test2(session))
+
+    await session.commit()
+    await session.close()
+
+    reset_session_context(context=context)
+
+    # await drop_db()
+
+
+async def test1(session):
+    print("debug======================", get_session_id())
+    session.add(Item(name="test1"))
+
+
+async def test2(session):
+    print("debug======================", get_session_id())
+    session.add(Item(name="test2"))
+
+
 if __name__ == "__main__":
-    asyncio.run(test())
+    asyncio.run(async_db_test())
+    asyncio.run(async_db_test())
